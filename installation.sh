d47f245abe531e8a740eb7d79760f8be1b989f47
Bauke Brenninkmeijer <bauke.brenninkmeijer@gmail.com>
2024-02-17 17:41:59 +0100

    change install

--- installation.sh
+++ installation.sh
@@ -8,3 +8,7 @@ chmod 700 get_helm.sh
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+helm repo add airflow-stable https://airflow-helm.github.io/charts
+helm repo update
